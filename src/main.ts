76b0c50290c1a25c9fbd1125af59d8bf8d509873
Fuuma <masa81hiro69@gmail.com>
2024-02-01 15:44:33 +0900

    chore: nestjs/swagger 追加

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('DQ Weapons API')
+    .setDescription('The DQ Weapons API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.use(cookieParser());
 
   await app.listen(3004);
