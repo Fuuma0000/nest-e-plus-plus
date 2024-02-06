7d79614173768c42890faa50e47a87e60e923c8c
Fuuma <masa81hiro69@gmail.com>
2024-02-07 06:59:37 +0900

    feat: genarate resource events

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from 'prisma/prisma.service';
+import { EventsResolver } from './events/events.resolver';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, EventsModule],
   controllers: [],
-  providers: [PrismaService],
+  providers: [PrismaService, EventsResolver],
   exports: [PrismaService],
 })
 export class AppModule {}
