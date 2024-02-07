c8e18655e0b8d70eef1b3366d3d596f0ef3b441a
Fuuma <masa81hiro69@gmail.com>
2024-02-07 09:58:03 +0900

    fix: 間違えてResolver入ってたので削除

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from 'prisma/prisma.service';
-import { EventsResolver } from './events/events.resolver';
 import { EventsModule } from './events/events.module';
 
 @Module({
   imports: [UsersModule, AuthModule, EventsModule],
   controllers: [],
-  providers: [PrismaService, EventsResolver],
+  providers: [PrismaService],
   exports: [PrismaService],
 })
 export class AppModule {}
